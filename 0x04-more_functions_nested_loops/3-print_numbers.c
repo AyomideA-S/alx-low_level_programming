5b1b13d5ad87aaf25fc519d8afaaeae56d2e9c1b
AyomideA-S <midesuperbest@gmail.com>
2022-07-07 16:36:12 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the digits 1 through 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar(10);
+}
