6c6271474e89b522f5d36cc14835e9b860fae9ae
AyomideA-S <midesuperbest@gmail.com>
2022-07-15 13:17:04 -0700

    Task 9 alternative

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,38 +6,26 @@
  */
 void print_number(int n)
 {
-	int flag;
-	unsigned int i;
+	unsigned int i, x, flag;
 
-	flag = 0;
-	i = 0;
-	if (n == 0)
+	if (n < 0)
 	{
-		_putchar(48);
+		_putchar(45);
+		i = -n;
 	}
 	else
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			n = -n;
-		}
-		if (n % 10 == 0)
-			flag = 1;
-		while (n)
-		{
-			if (n % 10 == 0 && flag)
-				flag++;
-			i = (i * 10) + (n % 10);
-			n /= 10;
-		}
-		while (i)
-		{
-			_putchar((i % 10) + 48);
-			i /= 10;
-		}
-		flag--;
-		while (flag-- > 0)
-			_putchar(48);
+		i = n;
+	}
+	x = i;
+	flag = 1;
+	while (i >= 10)
+	{
+		i /= 10;
+		flag *= 10;
+	}
+	for (; flag >= 1; flag /= 10)
+	{
+		_putchar(((x / flag) % 10) + 48);
 	}
 }
