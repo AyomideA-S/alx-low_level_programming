63e98fd17d71373f85496304688687731a8c8386
AyomideA-S <midesuperbest@gmail.com>
2022-07-20 21:34:06 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *power_operation - Returns the natural square root of a number
+ * @n: Integer
+ * @c: Iteration count
+ * Return: Natural square root or -1
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: Integer
+ * Return: Natural square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
