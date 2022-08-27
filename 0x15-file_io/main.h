c00700bbf5abdec5f2c6cae3e202019cf0759885
AyomideA-S <midesuperbest@gmail.com>
2022-08-27 22:46:59 +0100

    Tread lightly, she is near

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
