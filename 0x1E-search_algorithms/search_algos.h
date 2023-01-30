70fdf72207b9c45750b486e28020ddc4e4d736b2
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2023-01-30 05:46:32 +0100

    Linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,45 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
